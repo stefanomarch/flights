1b1a92681409803adc568b69b6e6d41a5e0fc2ce
stefano marchetti <stefano.marchetti20@gmail.com>
2021-04-03 11:06:43 +0200

    seed 5 airlplanes

--- db/migrate/20210402163926_create_airplanes.rb
+++ db/migrate/20210402163926_create_airplanes.rb
@@ -8,6 +8,8 @@ class CreateAirplanes < ActiveRecord::Migration[6.1]
       t.integer :avilables_seats
 
       t.timestamps
+
+       validates :model, :company, :rows, :columns, presence: true
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Airplane.destroy_all
+
+p1 = Airplane.create!(
+  model: 'Boeing 747',
+  company: 'KLM',
+  rows: 8,
+  columns: 4)
+p2 = Airplane.create!(
+  model:'Airbus 350-100',
+  company: 'Lufthansa',
+  rows: 5,
+  columns: 7)
+p3 = Airplane.create!(
+  model: 'Airbus A380',
+  company: 'Fly-Emirates',
+  rows: 6,
+  columns: 2)
+p4 = Airplane.create!(
+  model: 'Boeing 737',
+  company: 'Ryanair',
+  rows: 10,
+  columns: 9)
+p5 = Airplane.create!(
+  model: 'Emabaer 175',
+  company: 'Vueling',
+  rows: 5,
+  columns: 5)
+
+puts 'airplane seeds done 😎'
